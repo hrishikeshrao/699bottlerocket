15ac7191c323d56857e4ce8d58e2843e728d93e9
Wilson Parson <wilsonparson@gmail.com>
2017-12-05 00:19:50 -0500

    Create modal and form to create polls
    * Delete many of Erik's template files.
    * Reorganize base.html and Jinja templates.

--- static/app.js
+++ static/app.js
@@ -1,20 +1,63 @@
 'use strict';
 
-var app = angular.module('app', []);
+var app = angular.module('AorB',[]);
 
-app.controller('feed', ['$scope', 'pollsService', function($scope, pollsService) {
-  pollsService.getPolls().then((response) => {
-    $scope.feed = response.data;
+app.controller('feed', ['$scope', '$rootScope', 'pollsService', function($scope, $rootScope, pollsService) {
+
+  $scope.update = () => {
+    pollsService.getPolls().then((response) => {
+      $scope.feed = response.data;
+    });
+  };
+
+  $rootScope.$on('updateFeed', () => {
+    $scope.update();
   });
+
+  $scope.update();
+
 }]);
 
+app.controller('newPoll', ['$scope', '$rootScope', 'pollsService', function($scope, $rootScope, pollsService) {
+
+  $scope.createPoll = (form) => {
+    if (form.$valid) {
+      pollsService.createPoll($scope.newPoll).then((response) => {
+        $scope.resetForm(form);
+        $scope.dismissModal();
+        $rootScope.$emit('updateFeed', {});
+      });
+    }
+  }
+
+  $scope.resetForm = (form) => {
+    $scope.newPoll = {};
+    form.$setPristine();
+    form.$setUntouched();
+  }
+
+  $scope.dismissModal = () => {
+        var modalInstance = angular.element(document.querySelector('#modal-create-poll'));
+        modalInstance.modal('toggle');
+  }
+
+}]);
+
+
 app.factory('pollsService', ['$http', function($http){
+
   function getPolls() {
-    return $http.get('/showpoll_ng')
+    return $http.get('/showpoll_ng/')
+    .then((response) => response);
+  }
+
+  function createPoll(data) {
+    return $http.post('/create_poll_ng/', data)
     .then((response) => response);
   }
 
   return {
-    getPolls: getPolls
+    getPolls: getPolls,
+    createPoll: createPoll
   };
 }]);
